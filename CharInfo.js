113e31f93bdbb3292d2da8108d8076c39b86ac94
SIGNAL-LOST <32801301+SIGNAL-LOST@users.noreply.github.com>
2023-05-20 11:48:17 -0400

    Updated for trio work

--- CharInfo.js
+++ CharInfo.js
@@ -56,171 +56,229 @@ newCharacter.prototype.synergyWith = function(otherChar) {
     }
 }
 
-const char1 = new newCharacter(
-    c.Wulf,
-    [c.BBand, c.Rob, c.Umbrella],
-    [c.Bella, c.Double, c.Liz, c.Fukua, c.Soul, c.Squig, c.Anne],
-    [c.Fil, c.Pain, c.Peak, c.Val],
-    []
-);
 
-const char2 = new newCharacter(
-    c.BBand,
-    [c.Double, c.Pain, c.Soul, c.Rob, c.Squig, c.Anne],
-    [c.Wulf, c.Bella, c.Liz, c.Fil, c.Fukua, c.Peak, c.Val, c.Umbrella],
-    [],
-    [c.Fortune]
-);
+const characterList = {
+    "None": 0,
 
-const char3 = new newCharacter(
-    c.Bella,
-    [c.BBand, c.Double, c.Rob],
-    [c.Liz, c.Fil, c.Fukua, c.Pain, c.Soul, c.Peak, c.Squig, c.Val, c.Umbrella, c.Anne],
-    [c.Wulf],
-    [c.Fortune]
-);
+    "Wulf": char1 = new newCharacter(
+        c.Wulf,
+        [c.BBand, c.Rob, c.Umbrella],
+        [c.Bella, c.Double, c.Liz, c.Fukua, c.Soul, c.Squig, c.Anne],
+        [c.Fil, c.Pain, c.Peak, c.Val],
+        []
+    ),
 
-const char4 = new newCharacter(
-    c.Double,
-    [c.BBand, c.Liz, c.Fukua, c.Soul, c.Rob],
-    [c.Wulf, c.Bella, c.Fil, c.Pain, c.Peak, c.Squig, c.Val, c.Umbrella, c.Anne],
-    [c.Fortune],
-    []
-);
+    "BBand": char2 = new newCharacter(
+        c.BBand,
+        [c.Double, c.Pain, c.Soul, c.Rob, c.Squig, c.Anne],
+        [c.Wulf, c.Bella, c.Liz, c.Fil, c.Fukua, c.Peak, c.Val, c.Umbrella],
+        [],
+        [c.Fortune]
+    ),
 
-const char5 = new newCharacter(
-    c.Liz,
-    [c.BBand, c.Double, c.Fukua, c.Rob, c.Squig],
-    [c.Bella, c.Fil, c.Soul, c.Peak, c.Anne],
-    [c.Wulf, c.Fortune, c.Pain, c.Umbrella],
-    [c.Val]
-);
+    "Bella": char3 = new newCharacter(
+        c.Bella,
+        [c.BBand, c.Double, c.Rob],
+        [c.Liz, c.Fil, c.Fukua, c.Pain, c.Soul, c.Peak, c.Squig, c.Val, c.Umbrella, c.Anne],
+        [c.Wulf],
+        [c.Fortune]
+    ),
 
-const char6 = new newCharacter(
-    c.Fil,
-    [c.BBand, c.Bella, c.Double, c.Liz, c.Rob],
-    [c.Fukua, c.Pain, c.Soul, c.Peak, c.Squig, c.Umbrella, c.Anne],
-    [c.Wulf],
-    [c.Fortune, c.Val]
-);
+    "Double": char4 = new newCharacter(
+        c.Double,
+        [c.BBand, c.Liz, c.Fukua, c.Soul, c.Rob],
+        [c.Wulf, c.Bella, c.Fil, c.Pain, c.Peak, c.Squig, c.Val, c.Umbrella, c.Anne],
+        [c.Fortune],
+        []
+    ),
 
-const char7 = new newCharacter(
-    c.Fortune,
-    [c.BBand, c.Bella, c.Double, c.Liz, c.Soul, c.Rob, c.Squig, c.Umbrella, c.Anne],
-    [c.Fil, c.Fukua, c.Pain, c.Peak],
-    [c.Wulf, c.Val],
-    []
-);
+    "Liz": char5 = new newCharacter(
+        c.Liz,
+        [c.BBand, c.Double, c.Fukua, c.Rob, c.Squig],
+        [c.Bella, c.Fil, c.Soul, c.Peak, c.Anne],
+        [c.Wulf, c.Fortune, c.Pain, c.Umbrella],
+        [c.Val]
+    ),
 
-const char8 = new newCharacter(
-    c.Fukua,
-    [c.BBand, c.Bella, c.Double, c.Pain, c.Rob, c.Umbrella, c.Anne],
-    [c.Liz, c.Fil, c.Soul, c.Peak, c.Squig],
-    [c.Wulf, c.Val],
-    [c.Fortune]
-);
+    "Fil": char6 = new newCharacter(
+        c.Fil,
+        [c.BBand, c.Bella, c.Double, c.Liz, c.Rob],
+        [c.Fukua, c.Pain, c.Soul, c.Peak, c.Squig, c.Umbrella, c.Anne],
+        [c.Wulf],
+        [c.Fortune, c.Val]
+    ),
 
-const char9 = new newCharacter(
-    c.Pain,
-    [c.BBand, c.Bella, c.Double, c.Rob],
-    [c.Fil, c.Fortune, c.Fukua, c.Soul, c.Peak, c.Umbrella, c.Anne],
-    [c.Wulf, c.Liz, c.Squig],
-    [c.Val]
-);
+    "Fortune": char7 = new newCharacter(
+        c.Fortune,
+        [c.BBand, c.Bella, c.Double, c.Liz, c.Soul, c.Rob, c.Squig, c.Umbrella, c.Anne],
+        [c.Fil, c.Fukua, c.Pain, c.Peak],
+        [c.Wulf, c.Val],
+        []
+    ),
 
-const char10 = new newCharacter(
-    c.Soul,
-    [c.BBand, c.Bella, c.Double, c.Fukua, c.Pain, c.Rob],
-    [c.Liz, c.Fil, c.Peak, c.Squig, c.Umbrella, c.Anne],
-    [c.Wulf, c.Fortune],
-    [c.Val]
-);
+    "Fukua": char8 = new newCharacter(
+        c.Fukua,
+        [c.BBand, c.Bella, c.Double, c.Pain, c.Rob, c.Umbrella, c.Anne],
+        [c.Liz, c.Fil, c.Soul, c.Peak, c.Squig],
+        [c.Wulf, c.Val],
+        [c.Fortune]
+    ),
 
-const char11 = new newCharacter(
-    c.Peak,
-    [c.BBand, c.Bella, c.Double, c.Fil, c.Rob, c.Umbrella, c.Anne],
-    [c.Wulf, c.Liz, c.Fukua, c.Pain, c.Soul, c.Squig],
-    [c.Fortune],
-    [c.Val]
-);
+    "Pain": char9 = new newCharacter(
+        c.Pain,
+        [c.BBand, c.Bella, c.Double, c.Rob],
+        [c.Fil, c.Fortune, c.Fukua, c.Soul, c.Peak, c.Umbrella, c.Anne],
+        [c.Wulf, c.Liz, c.Squig],
+        [c.Val]
+    ),
 
-const char12 = new newCharacter(
-    c.Rob,
-    [c.BBand, c.Double, c.Liz, c.Fil, c.Umbrella, c.Anne],
-    [c.Wulf, c.Bella, c.Fortune, c.Fukua, c.Pain, c.Soul, c.Peak, c.Squig, c.Val],
-    [],
-    []
-);
+    "Soul": char10 = new newCharacter(
+        c.Soul,
+        [c.BBand, c.Bella, c.Double, c.Fukua, c.Pain, c.Rob],
+        [c.Liz, c.Fil, c.Peak, c.Squig, c.Umbrella, c.Anne],
+        [c.Wulf, c.Fortune],
+        [c.Val]
+    ),
 
-const char13 = new newCharacter(
-    c.Squig,
-    [c.BBand, c.Rob],
-    [c.Wulf, c.Bella, c.Double, c.Liz, c.Fil, c.Fukua, c.Soul, c.Peak, c.Val, c.Umbrella, c.Anne],
-    [c.Fortune, c.Pain],
-    []
-);
+    "Peak": char11 = new newCharacter(
+        c.Peak,
+        [c.BBand, c.Bella, c.Double, c.Fil, c.Rob, c.Umbrella, c.Anne],
+        [c.Wulf, c.Liz, c.Fukua, c.Pain, c.Soul, c.Squig],
+        [c.Fortune],
+        [c.Val]
+    ),
 
-const char14 = new newCharacter(
-    c.Val,
-    [c.BBand, c.Bella, c.Double, c.Rob],
-    [c.Liz, c.Fil, c.Fukua, c.Soul, c.Squig, c.Umbrella, c.Anne],
-    [c.Wulf, c.Peak],
-    [c.Fortune, c.Pain]
-);
+    "Rob": char12 = new newCharacter(
+        c.Rob,
+        [c.BBand, c.Double, c.Liz, c.Fil, c.Umbrella, c.Anne],
+        [c.Wulf, c.Bella, c.Fortune, c.Fukua, c.Pain, c.Soul, c.Peak, c.Squig, c.Val],
+        [],
+        []
+    ),
 
-const char15 = new newCharacter(
-    c.Anne,
-    [c.BBand, c.Rob],
-    [c.Wulf, c.Bella, c.Double, c.Liz, c.Fil, c.Fukua, c.Soul, c.Peak, c.Squig, c.Umbrella],
-    [c.Fortune, c.Pain, c.Val],
-    []
-);
+    "Squig": char13 = new newCharacter(
+        c.Squig,
+        [c.BBand, c.Rob],
+        [c.Wulf, c.Bella, c.Double, c.Liz, c.Fil, c.Fukua, c.Soul, c.Peak, c.Val, c.Umbrella, c.Anne],
+        [c.Fortune, c.Pain],
+        []
+    ),
 
-const char16 = new newCharacter(
-    c.Umbrella,
-    [c.BBand, c.Fil, c.Rob],
-    [c.Bella, c.Double, c.Liz, c.Fukua, c.Pain, c.Soul, c.Peak, c.Squig, c.Anne],
-    [c.Wulf, c.Fortune],
-    [c.Val]
-);
+    "Val": char14 = new newCharacter(
+        c.Val,
+        [c.BBand, c.Bella, c.Double, c.Rob],
+        [c.Liz, c.Fil, c.Fukua, c.Soul, c.Squig, c.Umbrella, c.Anne],
+        [c.Wulf, c.Peak],
+        [c.Fortune, c.Pain]
+    ),
 
-/*
-const char17 = new newCharacter(
-    "Black Dahlia",
-    [""],
-    [""],
-    [""],
-    [""]
-)
-*/
+    "Anne": char15 = new newCharacter(
+        c.Anne,
+        [c.BBand, c.Rob],
+        [c.Wulf, c.Bella, c.Double, c.Liz, c.Fil, c.Fukua, c.Soul, c.Peak, c.Squig, c.Umbrella],
+        [c.Fortune, c.Pain, c.Val],
+        []
+    ),
 
-/*
-const char18 = new newCharacter(
-    "Marie",
-    [""],
-    [""],
-    [""],
-    [""]
-)
-*/
+    "Umbrella": char16 = new newCharacter(
+        c.Umbrella,
+        [c.BBand, c.Fil, c.Rob],
+        [c.Bella, c.Double, c.Liz, c.Fukua, c.Pain, c.Soul, c.Peak, c.Squig, c.Anne],
+        [c.Wulf, c.Fortune],
+        [c.Val]
+    )
 
-const characterList = [char1, char2, char3, char4, char5, char6, char7, char8, char9, char10, char11, char12, char13, char14, char15, char16];
+    /*
+    const char17 = new newCharacter(
+        "Black Dahlia",
+        [""],
+        [""],
+        [""],
+        [""]
+    )
+    */
 
-let select1 = undefined;
-let select2 = undefined;
+    /*
+    const char18 = new newCharacter(
+        "Marie",
+        [""],
+        [""],
+        [""],
+        [""]
+    )
+    */
+}
+
+let select1 = null;
+let select2 = null;
+let select3 = null;
 
 function showSynergy()
 {
-    select1 = characterList[document.getElementById("FighterIn").value - 1];
-    select2 = characterList[document.getElementById("AssistIn").value - 1];
+    select1 = characterList[document.getElementById("Fighter1In").value];
+    select2 = characterList[document.getElementById("Fighter2In").value];
+    select3 = characterList[document.getElementById("Fighter3In").value];
 
-    console.log(select1);
-    console.log(select2);
+    document.getElementById("Fighter1Out").innerHTML = `Fighter: ${select1.charName}`;
+    document.getElementById("Fighter2Out").innerHTML = `Fighter: ${select2.charName}`;
+    document.getElementById("Fighter3Out").innerHTML = `Fighter: ${select3.charName}`;
 
-    if (select1 != undefined && select2 != undefined)
+    let synergy1 = "";
+    let synergy2 = "";
+    let synergy3 = "";
+
+    if (select1)
     {
-        document.getElementById("FighterOut").innerHTML = `Fighter: ${select1.charName}`;
-        document.getElementById("AssistOut").innerHTML = `Assist: ${select2.charName}`;
-        document.getElementById("SynergyOut").innerHTML = `Synergy: ${select1.synergyWith(select2)}`;
+        if (select2)
+        {
+            synergy1 += `<br>${select1.charName} with ${select2.charName} assist: ${select1.synergyWith(select2)}`;
+        }
+        if (select3)
+        {
+            synergy1 += `<br>${select1.charName} with ${select3.charName} assist: ${select1.synergyWith(select3)}`;
+        }
+        document.getElementById("Synergy1Out").innerHTML = `Synergies: ${synergy1}`;
     }
+    else
+    {
+        document.getElementById("Fighter1Out").innerHTML = ``;
+        document.getElementById("Synergy1Out").innerHTML = ``;
+    }
+
+    if (select2)
+    {
+        if (select1)
+        {
+            synergy2 += `<br>${select2.charName} with ${select1.charName} assist: ${select2.synergyWith(select1)}`;
+        }
+        if (select3)
+        {
+            synergy2 += `<br>${select2.charName} with ${select3.charName} assist: ${select2.synergyWith(select3)}`;
+        }
+        document.getElementById("Synergy2Out").innerHTML = `Synergies: ${synergy2}`;
+    }
+    else
+    {
+        document.getElementById("Fighter2Out").innerHTML = ``;
+        document.getElementById("Synergy2Out").innerHTML = ``;
+    }
+
+    if (select3)
+    {
+        if (select1)
+        {
+            synergy3 += `<br>${select3.charName} with ${select1.charName} assist: ${select3.synergyWith(select1)}`;
+        }
+        if (select2)
+        {
+            synergy3 += `<br>${select3.charName} with ${select2.charName} assist: ${select3.synergyWith(select2)}`;
+        }
+        document.getElementById("Synergy3Out").innerHTML = `Synergies: ${synergy3}`;
+    }
+    else
+    {
+        document.getElementById("Fighter3Out").innerHTML = ``;
+        document.getElementById("Synergy3Out").innerHTML = ``;
+    }
+
 }
